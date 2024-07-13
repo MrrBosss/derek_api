fa25ddf8fae4cd92e6a683de1fdab83035b80a08
Boss <abdumuratovabduvali@07gmail.com>
2024-07-13 14:20:55 +0500

    brands modeltranslation fixed

--- products/translations.py
+++ products/translations.py
@@ -1,6 +1,6 @@
 from modeltranslation.translator import TranslationOptions
 from modeltranslation.decorators import register
-from .models import Product, FAQ, Banner , Brand
+from .models import Product, FAQ, Banner
 from . models import Category
 
 
@@ -20,12 +20,6 @@ class BannerTranslationOptions(TranslationOptions):
 @register(FAQ)
 class FAQTranslationOptions(TranslationOptions):
     fields = ('question', 'answer')
-
-
-
-@register(Brand)
-class BrandTranslationOptions(TranslationOptions):
-    fields = ('name',)
 
 
 
