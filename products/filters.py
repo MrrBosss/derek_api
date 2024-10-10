d9379b9277dc982ea2f0e97b4744d36286d13e12
Boss <abdumuratovabduvali@07gmail.com>
2024-10-10 09:46:42 +0500

    filters.py fixed

--- products/filters.py
+++ products/filters.py
@@ -1,20 +1,19 @@
 import django_filters
 from django import forms
 from django.db import models
-from .models import Product, ProductColor, ProductWeight, Category
-
+from .models import Product, ProductColor, ProductWeight, Category, ProductPrice
 
 
 class ProductFilter(django_filters.FilterSet):
-    min_price = django_filters.NumberFilter(method='filter_by_weight', lookup_expr='gte')
-    max_price = django_filters.NumberFilter(method='filter_by_weight', lookup_expr='lte')
+    min_price = django_filters.NumberFilter(field_name='price__amount', lookup_expr='gte', label='Min Price')
+    max_price = django_filters.NumberFilter(field_name='price__amount', lookup_expr='lte', label='Max Price')
     
     available = django_filters.BooleanFilter(method='filter_by_availability')
 
-    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains', label='Title')
 
     color = django_filters.ModelMultipleChoiceFilter(
-        field_name='color', 
+        field_name='price__color',  # Now filtering based on ProductPrice's color
         queryset=ProductColor.objects.all(),
         label='Color'
     )
@@ -26,26 +25,21 @@ class ProductFilter(django_filters.FilterSet):
     )
 
     weight = django_filters.ModelMultipleChoiceFilter(
+        field_name='price__weight',  # Now filtering based on ProductPrice's weight
         queryset=ProductWeight.objects.all(),
-        method='filter_by_weight',
         widget=forms.Select(attrs={'onchange': 'this.form.submit();'}),
         label='Select Weight'
     )
 
-    def filter_by_weight(self, queryset, name, value):
-        if value:
-            # Assuming `ProductWeight` model has a `price` field for specific weight prices
-            return queryset.annotate(price_adjusted=models.F('price') * value.price)
-        return queryset
-
-
     def filter_by_availability(self, queryset, name, value):
         if value:
-            return queryset.filter(stock__gt=0)  # Available products
+            # Filter products whose related ProductPrice has stock > 0
+            return queryset.filter(price__stock__gt=0).distinct()
         else:
-            return queryset.filter(stock__lte=0)  # Non-available products
+            return queryset.filter(price__stock__lte=0).distinct()
 
     class Meta:
         model = Product
-        fields = ['min_price', 'max_price', 'available','title', 'weight', 'color', 'category']
+        fields = ['min_price', 'max_price', 'available', 'title', 'weight', 'color', 'category']
+
 
