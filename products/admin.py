6fe2384df61effc6e0121fefac54771ac584783c
STQT <productgsm@gmail.com>
2025-12-03 16:33:49 +0500

    admin panel show shots

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 # Register your models here.
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Product, ProductWeight, FAQ, Banner, Brand, Category, Order, ProductPrice
 from .models import ProductColor, Catalog, OrderItem, Team, BestSeller, ProductShots
 
@@ -24,6 +25,13 @@ class OrderAdmin(admin.ModelAdmin):
 class ProductShotsInline(admin.TabularInline):
     model = ProductShots
     extra = 0
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-width: 100px; max-height: 100px;" />', obj.image.url)
+        return "No image"
+    image_preview.short_description = 'Preview'
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
